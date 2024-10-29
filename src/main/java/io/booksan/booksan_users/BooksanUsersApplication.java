d297044f4020d075a6c6db354913904147750831
MincheolSin <smc970417@gmail.com>
2024-10-29 17:14:27 +0900

    first

---
+++ src/main/java/io/booksan/booksan_users/BooksanUsersApplication.java
@@ -0,0 +1,13 @@
+package io.booksan.booksan_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksanUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksanUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/booksan/booksan_users/BooksanUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.booksan.booksan_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksanUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
