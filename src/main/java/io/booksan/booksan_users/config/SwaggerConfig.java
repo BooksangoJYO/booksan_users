ca5282d464f8ee07cc6c1a376f02f65f7168a264
ggwag <gdh6356@gmail.com>
2024-11-02 15:30:26 +0900

    feat: 소셜 로그인 토큰 받아오기

--- src/main/java/io/booksan/booksan_users/config/SwaggerConfig.java
+++ src/main/java/io/booksan/booksan_users/config/SwaggerConfig.java
@@ -3,12 +3,12 @@ package io.booksan.booksan_users.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
-
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
