3c3271f936f3e03ac48ce4e50a15ed4e4c4a9f8b
ggwag <gdh6356@gmail.com>
2024-11-18 14:00:10 +0900

    Refactor: 유저 어노테이션 수정

--- src/main/java/io/booksan/booksan_users/config/security/SecurityConfig.java
+++ src/main/java/io/booksan/booksan_users/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.booksan.booksan_users.config.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,40 +14,44 @@ import io.booksan.booksan_users.config.auth.PrincipalDetailsService;
 import io.booksan.booksan_users.config.jwt.JWTUtil;
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-	
+
 	private final JWTUtil jwtUtil;
-    private final PrincipalDetailsService principalDetailsService;
-    
+	private final PrincipalDetailsService principalDetailsService;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	       http
-	           .sessionManagement(session -> session
-	               .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	           )
-	           .csrf(csrf -> csrf.disable())
-	           .authorizeHttpRequests(matchers -> matchers
-	               .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-	               .requestMatchers(
-	                   "/",
-	                   "/api/**",
-	                   "/js/**",
-	                   "/css/**",
-	                   "/images/**"
-	               ).permitAll()
-	               .anyRequest().authenticated()
-	           );
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(matchers -> matchers.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+						.requestMatchers("/", "/api/**", "/js/**", "/css/**", "/images/**").permitAll().anyRequest()
+						.authenticated());
 //	           .addFilterBefore(
 //	               new TokenCheckFilter(jwtUtil, principalDetailsService),
 //	               UsernamePasswordAuthenticationFilter.class
 //	           );
 
-	       return http.build();
-	   }
-	
-	
+		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOriginPatterns(Arrays.asList("localhost:5173")); // 실제 운영환경에서는 구체적인 도메인 지정 필요
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 }
